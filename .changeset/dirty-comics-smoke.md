ba8e43e239517c09628d1f933c828bc9b056ebc6
Alex Graham <destgraham@icloud.com>
2024-07-09 14:58:58 -0700

    vite dependency

---
+++ .changeset/dirty-comics-smoke.md
@@ -0,0 +1,5 @@
+---
+"@ag108/bedtime-stories": patch
+---
+
+added vite dependency for sample cover images
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+	plugins: [
+		react(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'node_modules/@ag108/bedtime-stories/dist/covers',
+					dest: 'assets',
+				},
+			],
+		}),
+	],
+	build: {
+		assetsInlineLimit: 0,
+	},
+});
